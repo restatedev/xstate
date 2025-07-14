02343dcf8177f3ced23b81d67ca9141ea6b16390
Nik <nik@restate.dev>
2025-07-14 23:28:52 +0100

    Fix scripts

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,35 +1,9 @@
 import { defineConfig } from "vitest/config";
-import path from "path";
-
-const isWatch =
-  (process.argv.includes("--watch") || process.argv.includes("watch")) &&
-  !process.argv.includes("run");
 
 export default defineConfig(() => {
   return {
     test: {
-      globals: true,
-      environment: "node",
       projects: ["packages/*"],
-      watch: isWatch,
-      passWithNoTests: true,
     },
-    ...(isWatch && {
-      resolve: {
-        alias: {
-          "@restatedev/xstate": path.resolve(
-            __dirname,
-            "./packages/restate-xstate/src/index.ts",
-          ),
-          "@restatedev/xstate-test": path.resolve(
-            __dirname,
-            "./packages/restate-xstate-test/src/index.ts",
-          ),
-        },
-      },
-      optimizeDeps: {
-        exclude: ["@restatedev/xstate", "@restatedev/xstate-test"],
-      },
-    }),
   };
 });
